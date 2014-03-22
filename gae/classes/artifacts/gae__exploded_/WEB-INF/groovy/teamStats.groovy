4693cd3544f5c6ded38e6dad717e87200f4f8e63
Tobias Ericsson <tobias.ericsson@mobenga.com>
2014-03-23 00:25:16 +0100

    rm some stuff

--- gae/classes/artifacts/gae__exploded_/WEB-INF/groovy/teamStats.groovy
+++
@@ -1,7 +0,0 @@
-import logic.Scoreboard
-
-response.contentType = 'application/json'
-def teams = Scoreboard.teamStatsForLeagueYear("M1", 2014)
-
-def json = new groovy.json.JsonBuilder(teams)
-println json
